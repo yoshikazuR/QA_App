df4c362a3a01103633d396f373956c35343e7411
yoshikazuR <yoshikazu_takahashi@r.recuit.co.jp>
2021-09-29 09:25:54 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yoshikazu/takahashi/qa_app/Answer.kt
@@ -0,0 +1,4 @@
+package jp.techacademy.yoshikazu.takahashi.qa_app
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) : Serializable
---
+++ app/src/main/java/jp/techacademy/yoshikazu/takahashi/qa_app/Question.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yoshikazu.takahashi.qa_app
+import java.io.Serializable
+import java.util.ArrayList
+
+class Question(val title: String, val body: String, val name: String, val uid: String, val questionUid: String, val genre: Int, bytes: ByteArray, val answers: ArrayList<Answer>) : Serializable {
+    val imageBytes: ByteArray
+
+    init {
+        imageBytes = bytes.clone()
+    }
+}
